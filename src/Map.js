34f76b65b0e0b511e10c3ea5e217fb785da79840
krishansingh1 <kkrishan7074@gmail.com>
2021-12-25 18:39:00 +0530

    modified

--- src/Map.js
+++ src/Map.js
@@ -1,31 +1,36 @@
-import React, {useRef,useState,useEffect} from 'react'
-import mapboxgl from 'mapbox-gl'
-
-mapboxgl.accessToken='pk.eyJ1Ijoia3Jpc2hhbmlzc2luZ2giLCJhIjoiY2t4a2diMDk1MXdncTJ2bnBobnhnOHRodSJ9.Xdtp4fQI7BCxbTVrX12XYw'
+import React, {useState} from 'react'
+import ReactMapGl, {Marker} from 'react-map-gl'
+import './App.css'
+import * as mapData from './API/Area.json'
 
 function Map(){
+  const [viewport,setViewport]= useState({
+    latitude:12.972442,
+    longitude:77.580643,
+    zoom:10,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pitch:35
+  })
 
-    const [lng,setLng] = useState()
-    const [lat,setLat]= useState()
+  return (
+    <ReactMapGl 
+    mapboxApiAccessToken={'pk.eyJ1Ijoia3Jpc2hhbmlzc2luZ2giLCJhIjoiY2t4bGQ4dDA5MGJkeDJ3a290NmNicW9hOSJ9.4My0OZ5m3_LI8LSXSf8XvA'}
 
-    const containerRef= useRef()
-    const map= useRef(null)
-    
-    useEffect(()=>{
-      if(map.current) return;
-      map.current= new mapboxgl.Map({
-         container: mapContainer.current,
-         style: 'mapbox://styles/mapbox/streets-v11',
-         center: [lng,lat],
-         zoom:zoom,
-      })
-    })
+    mapStyle="mapbox://styles/mapbox/dark-v9"
+    {...viewport}
+    width='100vw'
+    height='100vh'
+    onViewportChange={(newView)=> setViewport(newView)}>
 
-  return(
-   <div>
-       <div className='map-container' ref={containerRef}></div>
-   </div>
+     {mapData.features.map((item)=>{
+       <Marker key={item.properties.pin_code} latitude={item.geometry.coordinates[0]} longitude={item.geometry.coordinates[1]}>
+         <button className='marker-btn'>
+           <img src="/public/heart_PNG51347.png" alt='heart icon'/>
+         </button>
+       </Marker>
+     })}     
+    </ReactMapGl>
   )
 }
-
 export default Map
